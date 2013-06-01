04ec0713b6113ccb51e2b73eac8ed437b4e596df
James Woglom <j@wogloms.net>
2013-06-01 14:50:24 -0400

    add static files

--- core/views.py
+++ core/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+def disp_homepage(request):
+    d = {'title': 'Intranet'}
+    return render_to_response('homepage.html', d)
+def disp_welcome(request):
+    d = {
+    	'title': 'Welcome, user',
+    	'header-title': 'Welcome, user'
+    }
+    return render_to_response('welcome.html', d)
+
+def blank_template(request):
+    return render_to_response('blank.html', {})
+def login_template(request):
+	return render_to_response('loginnew.html', {})
--- intranet3/urls.py
+++ intranet3/urls.py
@@ -6,12 +6,23 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'intranet3.views.home', name='home'),
-    # url(r'^intranet3/', include('intranet3.foo.urls')),
+    # url(r'^$', 'intranet.views.home', name='home'),
+    # url(r'^intranet/', include('intranet.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-      url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-      url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns+= patterns('core.views',
+    (r'^$', 'disp_homepage'),
+    (r'^welcome/$', 'disp_welcome'),
+    (r'^blanktemplate/$', 'blank_template'),
+    (r'^loginnew/$', 'login_template'),
+)
+urlpatterns+= patterns('auth.views',
+    (r'^login/$', 'disp_login'),
+    (r'^auth/$', 'disp_auth'),
 )
